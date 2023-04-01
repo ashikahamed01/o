1366317c76591eca5f5e369db1033d84ba2ef9af
ashikahamed01 <ahamedashk02@gmail.com>
2023-04-01 23:23:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+//const cors = require('cors');
+//const { getArticles, searchArticles } = require('./api');
+
+const app = express();
+
+const path= require('path');
+
+app.use(express.static(path.join(__dirname+"/public")));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
